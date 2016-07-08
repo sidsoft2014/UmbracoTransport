6590a1b43d4803ba39184121b2d75e41eb8cc7d1
sidsoft2014 <sidsoft2014@outlook.com>
2016-07-08 21:41:45 +0100

    initial commit

---
+++ BitcoinLib/Exchanges/PoloniexAPI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using BitcoinLib.Models;
+
+namespace BitcoinLib.Exchanges
+{
+    class PoloniexAPI
+    {
+        private const string _baseUrl = @" https://poloniex.com/public";
+        private readonly Dictionary<string, string> _commands = new Dictionary<string, string>
+        {
+            {"tickers", "returnTicker" }
+        };
+
+        public Dictionary<string, Ticker> GetTickers()
+        {
+            var url = $"{_baseUrl}+?command=returnTicker";
+            using(var wc = new WebClient())
+            {
+                try
+                {
+                    var json = wc.DownloadString(url);
+                    var tickers = JsonConvert.DeserializeObject<Dictionary<string, Ticker>>(json);
+                    return tickers;
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ BitcoinLib/Models/Ticker.cs
@@ -0,0 +1,31 @@
+using Newtonsoft.Json;
+
+namespace BitcoinLib.Models
+{
+    public class Ticker
+    {
+        /*
+            Example Data:
+            "last":"0.0251","lowestAsk":"0.02589999","highestBid":"0.0251","percentChange":"0.02390438",
+            "baseVolume":"6.16485315","quoteVolume":"245.82513926"
+        */
+
+        [JsonProperty("last")]
+        public decimal LastPrice { get; set; }
+
+        [JsonProperty("lowestAsk")]
+        public decimal LowestAsk { get; set; }
+
+        [JsonProperty("highestBid")]
+        public decimal HighestBid { get; set; }
+
+        [JsonProperty("percentChange")]
+        public decimal PercentChange { get; set; }
+
+        [JsonProperty("baseVolume")]
+        public decimal BaseVolume { get; set; }
+
+        [JsonProperty("quoteVolume")]
+        public decimal QuoteVolume { get; set; }
+    }
+}
