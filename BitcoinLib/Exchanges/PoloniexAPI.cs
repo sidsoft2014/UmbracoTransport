757d34925772a67f864e24f34c30ac007ef30a9a
sidsoft2014 <sidsoft2014@outlook.com>
2016-07-08 21:53:18 +0100

    added .gitignore rules

--- BitcoinLib/Exchanges/PoloniexAPI.cs
+++ BitcoinLib/Exchanges/PoloniexAPI.cs
@@ -25,6 +25,10 @@ namespace BitcoinLib.Exchanges
                 try
                 {
                     var json = wc.DownloadString(url);
+                    // When the API we are getting the JSON from changes it's format this will break.
+                    // When this happens check the JSON data to see what fields have been added, removed, changed name or had data type altered and then adjust the Ticker object to match.
+                    // If this is not the fix then the overall structure of the response must have changed. In this case, as well as the above, check the overall structure of the string.
+                    // (i.e. Is it still a Key-Value pair? Is the Key still a string? etc...)
                     var tickers = JsonConvert.DeserializeObject<Dictionary<string, Ticker>>(json);
                     return tickers;
                 }
